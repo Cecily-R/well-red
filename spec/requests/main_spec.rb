e35c74c113625c9321215a45f0432fed179a4c4a
Cecily Rowland <cec1rowland@gmail.com>
2023-04-19 15:27:54 +0100

    Initated Rspec

--- spec/requests/main_spec.rb
+++ spec/requests/main_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "HomePages", type: :request do
+RSpec.describe "Mains", type: :request do
   describe "GET /index" do
     pending "add some examples (or delete) #{__FILE__}"
   end
