fdfbdfa39f142ade78a2dc24998654ddbd6d7c79
Cecily Rowland <cec1rowland@gmail.com>
2023-04-27 12:06:07 +0100

    Added styling for gigs page

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -1,5 +1,6 @@
 class GigsController < ApplicationController
   def show
     @gigs = ContentfulClientService.new.client.entries(content_type: 'gig')
+    @gigs = @gigs.sort_by { |gig| gig.date_and_time }.reverse
   end
 end
