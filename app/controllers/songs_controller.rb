94053d7f248048c0eff682cbc904d3da0a032a17
Cecily Rowland <cec1rowland@gmail.com>
2023-05-04 09:08:15 +0100

    Altered CSS and finished song page

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,6 @@
 class SongsController < ApplicationController
   def show 
     @songs = ContentfulClientService.new.client.entries(content_type: 'songs')
+    @songs = @songs.sort_by { |song| song.title.downcase }
   end 
 end 
