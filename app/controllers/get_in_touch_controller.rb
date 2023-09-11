6cfd3e24380459761e20dbdcaba8a11c8e7173c8
Cecily Rowland <cec1rowland@gmail.com>
2023-09-11 20:43:36 +0100

    Try and get mails working

--- app/controllers/get_in_touch_controller.rb
+++ app/controllers/get_in_touch_controller.rb
@@ -4,10 +4,10 @@ class GetInTouchController < ApplicationController
   end
 
   def create
-    @contact = ContactMailer.contact_message(params[:name], params[:email], params[:message])
-    @valid_email = EmailValidator.valid?(@contact.from.first)
-
-    if @valid_email && @contact.deliver
+    @valid_email = EmailValidator.valid?(params[:email])
+    
+    if @valid_email
+      ActionMailer::Base.mail(to: 'cec1rowland@gmail.com', from: params[:email], subject: 'New Website Message', body: params[:message])
       flash[:alert] = "Thanks for getting in touch! We'll get back to you as soon as we can."
       redirect_to get_in_touch_path
     elsif !@valid_email
@@ -16,6 +16,6 @@ class GetInTouchController < ApplicationController
     else
       flash[:alert] = "Oops, something went wrong!"
       redirect_to get_in_touch_path
-    end 
+    end
   end 
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,7 @@ class ContactMailer < ApplicationMailer
   def self.contact_message(name, email, message)
     @name = name
     @message = message
+
     mail(to: 'cec1rowland@gmail.com', from: email, subject: 'New Website Message')
   end
 end
